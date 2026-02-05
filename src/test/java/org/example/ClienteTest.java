3db4161bbe983c1a42d127fef91db0dccf35ff60
-Alejandro Vallejos Vitalevi <alejandrovallejos123@gmail.com>
2026-02-05 20:40:06 -0300

    Tests

--- src/test/java/org/example/ClienteTest.java
+++ src/test/java/org/example/ClienteTest.java
@@ -1,17 +1,43 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.*;
 
 public class ClienteTest {
 
-
-    //pongo esto para ver si funcionan los tests
     @Test
-    public void suma() {
-        int suma = 2 + 3;
-        assertEquals(5, suma);
+    public void testAgregarYBuscarPorNombre() {
+        GestorClientes gestor = new GestorClientes();
+        gestor.agregarCliente("Alejadro", 75);
+
+        Cliente c = gestor.buscarPorNombre("Alejadro");
+        assertNotNull("Cliente agregado debe encontrarse por nombre", c);
+        assertEquals("Alejadro", c.getNombre());
+        assertEquals(75, c.getScoring());
+    }
+
+    @Test
+    public void testAgregarYBuscarPorScoring() {
+        GestorClientes gestor = new GestorClientes();
+        gestor.agregarCliente("Nicolas", 50);
+
+        Cliente c = gestor.buscarPorScoring(50);
+        assertNotNull("Cliente con scoring 50 debe encontrarse", c);
+        assertEquals("Nicolas", c.getNombre());
+        assertEquals(50, c.getScoring());
+    }
+
+    @Test
+    public void testHistorialRegistroAlAgregar() {
+        GestorClientes gestor = new GestorClientes();
+        gestor.agregarCliente("Gonzalo", 30);
+
+        HistorialAcciones h = gestor.getHistorial();
+        Accion a = h.deshacerAccion();
+        assertNotNull("Debe existir una acción en el historial después de agregar", a);
+        assertEquals("Agregar cliente", a.getTipo());
+        assertEquals("Gonzalo", a.getDetalle());
     }
 
 }
