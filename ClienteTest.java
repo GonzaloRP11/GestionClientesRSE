060f5b3c34e5715718cfcc4776e015b88d1dcb62
-Alejandro Vallejos Vitalevi <alejandrovallejos123@gmail.com>
2026-02-04 21:40:35 -0300

    Configuración para los tests

---
+++ ClienteTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class ClienteTest {
+
+
+    //pongo esto para ver si funcionan los tests
+    @Test
+    public void suma() {
+        int suma = 2 + 3;
+        assertEquals(5, suma);
+    }
+
+}
